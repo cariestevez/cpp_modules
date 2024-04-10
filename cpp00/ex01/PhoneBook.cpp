607255718a6769c3b10fb6ef74716187b546b3bc
Carina Estevez Orth <cestevez@c4c1c5.42berlin.de>
2024-04-10 18:49:29 +0200

    ex01 done

---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,44 @@
+#include "PhoneBook.hpp"
+
+void    PhoneBook::welcomeMessage(void) const
+{
+    std::cout	<< "\033[36m\n"
+				<< "Welcome to this crappy awesome ☎️📔! Please enter:\n"
+				<< "--->📝 ADD to save a new contact\n"
+				<< "--->🔍 SEARCH to display a specific contact info\n"
+				<< "--->👋 EXIT to close this phonebook"
+				<< "\033[0m" << std::endl; 
+}
+
+//creates the instances of Contact
+void PhoneBook::addContact(void)
+{
+    static int  i;
+
+    this->_contacts[i % MAX_CONTACTS].saveContact(i % MAX_CONTACTS);
+    i++;
+}
+
+void	PhoneBook::searchContact(void)
+{
+    int i = 0;
+
+    if (this->_contacts[i].getFirstName().empty())
+    {
+        std::cout << "\033[31mPhoneBook is empty! \033[0m🕳️" << std::endl;
+        return ;
+    }
+    while (i < 8 && !(this->_contacts[i].getFirstName().empty()))
+    {
+        this->_contacts[i].printPreview();
+        i++;
+    }
+    std::istringstream iss(this->_contacts[i].getUserInput ("\033[35m--->Enter index of desired contact to view full information: \033[0m"));
+    if (!(iss >> i) || !(i >= 0 && i < 8))
+    {
+        std::cout << "\033[31mError: Invalid input ❗️\033[0m" << std::endl;
+        return ;
+    }
+    this->_contacts[i].printContactFull();
+    return ;
+}
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -11,8 +11,8 @@ int main()
 		std::cin >> userInput; //reads only until the first whitespace
 		if (userInput.compare("ADD") == 0)
 			phonebook.addContact();
-		// else if (userInput.compare("SEARCH") == 0)
-		// 	phonebook.searchContact();
+		else if (userInput.compare("SEARCH") == 0)
+		 	phonebook.searchContact();
 		else if (userInput.compare("EXIT") == 0)
 			break ;
 	}
