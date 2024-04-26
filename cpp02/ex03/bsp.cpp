88de567e7a68f4d6917275e5876cc8d8c78fdc36
Carina Estevez Orth <cestevez@c4b9c6.42berlin.de>
2024-04-26 20:05:11 +0200

    learning maths from 0 at 33

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -4,15 +4,19 @@
 //take two axis of the triangle to create a plane dividing line
 //(edge of the triangle between this two coords is on the line)
 //returns true if the point is on the same side of the plane as the 3rd axis(=rest of the triangle)
-// bool    isOnSameSide(Point const &axisA, Point const &axisB, Point const &thirdAxis, Point const &point)
-// {
-    //Ax + By + C = 0
-    //doesn't work?!!!
-
-    //but works in this order?!!!
+bool    isOnSameSide(Point const &axisA, Point const &axisB, Point const &thirdAxis, Point const &point)
+{
+    // Ax + By + C = 0
+    // y = mx + c
+    // doesn't work?!!!
+    Fixed   A = axisB.getX() - axisA.getX();
+    Fixed   B = axisB.getY() - axisA.getY();
+   // but works in this order?!!!
     // Fixed   A = axisB.getY() - axisA.getY();
     // Fixed   B = axisA.getX() - axisB.getX();
-    // Fixed   C = Fixed(-1) * (A * axisA.getX() + B * axisA.getY());
+    Fixed   C = Fixed(-1) * (A * axisA.getX() + B * axisA.getY());
+  
+  
     // Fixed   slope;
     // slope = (axisB.getY() - axisA.getY()) / (axisB.getX() - axisA.getX());
 
@@ -24,8 +28,8 @@
     // A = Fixed(-1) / slope;
 
     // Fixed   C;
-    // Fixed   thirdAxisValue = A * thirdAxis.getX() + B * thirdAxis.getY() + C;
-    // Fixed   pointValue = A * point.getX() + B * point.getY() + C;
+    Fixed   thirdAxisValue = A * thirdAxis.getX() + B * thirdAxis.getY() + C;
+    Fixed   pointValue = A * point.getX() + B * point.getY() + C;
 
     // Fixed   anotherC = Fixed(-1) * (A * point.getX() + B * point.getY());
     // if (anotherC == C)
@@ -38,54 +42,28 @@
 
 
     // std::cout << "point value " << pointValue << std::endl;
-    // //printf("result of line equ of point: %d\n", pointValue.getRawBits());
-    // // float threshold = 0.001;
-    // // //float cero
-    // // // Tolerance threshold for zero comparison
-    // // Fixed tolerance = threshold; // Adjust as needed
+    //printf("result of line equ of point: %d\n", pointValue.getRawBits());
+    // float threshold = 0.001;
+    // //float cero
+    // // Tolerance threshold for zero comparison
+    // Fixed tolerance = threshold; // Adjust as needed
 
-    // // // Check if the result is within tolerance of zero
-    // // if (thirdAxisValue < tolerance && thirdAxisValue > Fixed(-1) * tolerance &&
-    // //     pointValue < tolerance && pointValue > Fixed(-1) * tolerance)
-    // //     return false;
-    // // // if line equation result is 0, point is on the line(=> outside or on edge of triangle)
-    // if (thirdAxisValue == 0 || pointValue == 0)
+    // // Check if the result is within tolerance of zero
+    // if (thirdAxisValue < tolerance && thirdAxisValue > Fixed(-1) * tolerance &&
+    //     pointValue < tolerance && pointValue > Fixed(-1) * tolerance)
     //     return false;
-    // //if both have the same sign, point is on the same side of the plane as the triangle
-    // if ((thirdAxisValue > 0.0f && pointValue > 0.0f) || (thirdAxisValue < 0.0f && pointValue < 0.0f))
-    //     return true;
-    // return false;
-//}
-
-
-
-Fixed   crossProduct(Point const &axisA, Point const &axisB, Point const &point)
-{
-    Fixed   abX = axisB.getX() - axisA.getX();
-    Fixed   abY = axisB.getY() - axisA.getY();
-    Fixed   apX = point.getX() - axisA.getX();
-    Fixed   apY = point.getY() - axisA.getY();
-
-    Fixed result = abX * apY - apX * abY;
-
-    return result;
-}
-
-bool bsp( Point const a, Point const b, Point const c, Point const point)
-{
-    Fixed resultAB = crossProduct(a, b, point);
-    Fixed resultBC = crossProduct(b, c, point);
-    Fixed resultCA = crossProduct(c, a, point);
-
-    if ((resultAB > 0 && resultBC > 0 && resultCA > 0) || (resultAB < 0 && resultBC < 0 && resultCA < 0))
-       return true;
+    // // if line equation result is 0, point is on the line(=> outside or on edge of triangle)
+    if (thirdAxisValue == 0 || pointValue == 0)
+        return false;
+    //if both have the same sign, point is on the same side of the plane as the triangle
+    if ((thirdAxisValue > 0.0f && pointValue > 0.0f) || (thirdAxisValue < 0.0f && pointValue < 0.0f))
+        return true;
     return false;
-  
 }
 
-//a, b and c hold the coordinates of the 3 axis of the triangle
-//if checking from each of the edges, the point is on the same plane as the rest of the triangle
-//the point can't but be inside of it
+// a, b and c hold the coordinates of the 3 axis of the triangle
+// if checking from each of the edges, the point is on the same plane as the rest of the triangle
+// the point can't but be inside of it
 // bool bsp( Point const a, Point const b, Point const c, Point const point)
 // {
 //     if (isOnSameSide(a, b, c, point) && isOnSameSide(b, c, a, point)
@@ -93,6 +71,32 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
 //         return true;
 //     return false;
 // }
+
+
+// Fixed   crossProduct(Point const &axisA, Point const &axisB, Point const &point)
+// {
+//     Fixed   abX = axisB.getX() - axisA.getX();
+//     Fixed   abY = axisB.getY() - axisA.getY();
+//     Fixed   apX = point.getX() - axisA.getX();
+//     Fixed   apY = point.getY() - axisA.getY();
+
+//     Fixed result = abX * apY - apX * abY;
+
+//     return result;
+// }
+
+// bool bsp( Point const a, Point const b, Point const c, Point const point)
+// {
+//     Fixed resultAB = crossProduct(a, b, point);
+//     Fixed resultBC = crossProduct(b, c, point);
+//     Fixed resultCA = crossProduct(c, a, point);
+
+//     if ((resultAB > 0 && resultBC > 0 && resultCA > 0) || (resultAB < 0 && resultBC < 0 && resultCA < 0))
+//        return true;
+//     return false;
+  
+// }
+
 
 /*
 1. Use the line equation to create a dividing line (one for each of the sides of the triangle)
