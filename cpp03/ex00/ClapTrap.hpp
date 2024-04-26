a7fda168ea9318afbdbc0b7d849871028a17b223
Carina Estévez Orth <carinaestevezorth@CariBook-Air.fritz.box>
2024-04-27 01:50:26 +0200

    cpp03 ex00 done?

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+private:
+    std::string          _name;
+    unsigned int         _hitPoints;
+    unsigned int         _energyPoints;
+    unsigned int         _attackDamage;
+public:
+    ClapTrap(void);
+    ~ClapTrap(void);
+    ClapTrap(std::string name);
+    
+    ClapTrap(const ClapTrap &original);
+    ClapTrap &operator=(const ClapTrap &original);
+
+    std::string     getName(void);
+    unsigned int	getHitPoints(void) const;
+    unsigned int	getEnergyPoints(void) const;
+    unsigned int    getAttackDamage(void) const;
+    
+    void setAttackDamage(unsigned int amount);
+
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+
+};
+
+#endif
