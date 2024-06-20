3ae8af0e56739154821a8114651a957a29d0963e
carina <carinaestevezorth@gmail.com>
2024-06-20 14:59:38 +0200

    ex00 done

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -2,28 +2,20 @@
 
 BitcoinExchange::BitcoinExchange(const std::string &dataBaseFile)
 {
-    std::cout << "\e[0;103mBitcoinExchange::\e[0m Default constructor called" << std::endl;
     loadDataBase(dataBaseFile);
 }
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &source)
-{
-    std::cout << "\e[0;103mBitcoinExchange::\e[0m Copy constructor called" << std::endl;
-}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &source) : exchangeRateMap(source.exchangeRateMap) {}
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &source)
 {
     if (this != &source)
-        //code
-	
-    std::cout << "\e[0;103mBitcoinExchange::\e[0m Assignment operator used" << std::endl;
+        exchangeRateMap = source.exchangeRateMap;
+
 	return (*this);
 }
 
-BitcoinExchange::~BitcoinExchange()
-{
-    std::cout << "\e[0;103mBitcoinExchange::\e[0m Destructor called" << std::endl;
-}
+BitcoinExchange::~BitcoinExchange() {}
 
 //////////////////////////////////////////////////////////////////////////////////////
 
@@ -37,6 +29,7 @@ void BitcoinExchange::loadDataBase(const std::string &dataBaseFile)
     }
 
     std::string line;
+    std::getline(db, line);
     while (std::getline(db, line))
     {
         bool validLine = false;
@@ -49,17 +42,18 @@ void BitcoinExchange::loadDataBase(const std::string &dataBaseFile)
             if (ss >> rate && iss.eof() && isValidDateFormat(date))//improve the conversion to float of the rate?
                 validLine = true;
         }
-        if (validLine)
+        if (validLine && date != "date")
             exchangeRateMap[date] = rate;
         else
-            std::cout << "Skipping invalid line in data base => " << line << std::endl;
+            std::cout << "Skipping (invalid) line in data base => " << line << std::endl;
     }
     
     db.close();
 
     if (exchangeRateMap.empty())
-        std::cout << "Error: No valid values could be extracted from database. Provide a valid one." << std::endl;
+        std::cout << "Error: No valid values could be extracted from database. Please provide a valid one." << std::endl;
     
+    //std::cout << "database parsed successfully" << std::endl;
     return ;
 
 }
@@ -70,20 +64,22 @@ void BitcoinExchange::processQuery(std::string &date, std::string &value)
 {
     float floatValue = std::stof(value);
 
-    // Check if the provided form name exists in the map
-    std::map<std::string, float>::const_iterator it = exchangeRateMap.find(date);
-	if (it != exchangeRateMap.end())//if it didn't arrive at the end => coincidence found
-	{
-        //exchange rate * floatValue
-        it->second * floatValue;
-        std::cout << it->first << " => " << it->second << " = " << it->second * floatValue << std::endl;
-		
-    } 
-	else
-	{
-        //search for the last exchange date and do the same as above
-        //else return error messages in case the calculation results are wrong, but at this point the input has been checked
-        //maybe do some more checks for the rate before saving into map?
+    std::map<std::string, float>::const_iterator iter = exchangeRateMap.lower_bound(date);
+
+    //std::cout << "return of lower bound iter: " << iter->first << std::endl;
+    if (iter->first == date)
+    {
+        std::cout << date << " => " << value << " = " << iter->second * floatValue << std::endl;
+    }
+    else if (iter->first > date && iter == exchangeRateMap.begin())
+    {
+        std::cout << "Error: date " << date << " is before the oldest date in the database." << std::endl;
+    }
+    else
+    {
+        // If no exact match, use the closest previous date
+        std::map<std::string, float>::const_iterator prev = std::prev(iter);
+        std::cout << date << " => " << value << " = " << prev->second * floatValue << std::endl;
     }
 }
 
@@ -92,22 +88,23 @@ void BitcoinExchange::processInputFile(std::ifstream &inputFile)
     std::string line;
     while (std::getline(inputFile, line))
     {
+        //std::cout << line << std::endl;
         std::istringstream iss(line);
         std::string date, separator, value;
-        if (!(iss >> date >> separator >> value) || !iss.eof() || separator != "|")//separes the line by whitespaces and saves in each of the variables
-        {
-            std::cout << "Error: bad formatted line." << std::endl;
-            continue;
-        }
-        if (date != "date" && !(isValidDateFormat(date) && isValidDate(date)))
+        if (!(iss >> date) || (date != "date" && !(isValidDateFormat(date) && isValidDate(date))))
         {
             std::cout << "Error: bad input => " << date << std::endl;
             continue;
         }
+        if (!(iss >> separator >> value) || !iss.eof() || separator != "|")//separes the line by whitespaces and saves in each of the variables
+        {
+            std::cout << "Error: bad formatted line." << std::endl;
+            continue;
+        }
         if (value != "value" && !(isValidValue(value)))
             continue;
-        
-        processQuery(date, value);
+        if (date != "date" && value != "value")
+            processQuery(date, value);
     }
 
     return;
@@ -128,21 +125,30 @@ bool BitcoinExchange::isValidDate(const std::string &date)
         return false;
     if (year < 2009 || year > 2024 || month < 1 || month > 12 || day < 1 || day > 31)
         return false;
+    
+    return true;
 }
 
 bool BitcoinExchange::isValidValue(const std::string &value)
 {
+    std::regex floatRegex("^[+-]?([0-9]*[.])?[0-9]+$");
+
+    if (!std::regex_match(value, floatRegex))
+    {
+        std::cout << "Error: number not of valid type." << std::endl;
+        return false;
+    }
     try
     {
         float floatValue = std::stof(value);
         if (floatValue < 0)
         {
-            std::cout << "Error! not a positive number." << std::endl;
+            std::cout << "Error: not a positive number." << std::endl;
             return false;
         }
         if (floatValue > 1000)
         {
-            std::cout << "Error! too large a number." << std::endl;
+            std::cout << "Error: too large a number." << std::endl;
             return false;
         }
     }
