bed3d04d3d68b5144ad61c45329cba2a928604a1
Carina Estevez Orth <cestevez@c4a11c10.42berlin.de>
2024-06-27 18:50:56 +0200

    hating it

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,39 +1,66 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe(void)
+PmergeMe::PmergeMe(std::vector<int> &userInput)
 {
-    std::cout << "\e[0;103mPmergeMe::\e[0m Default constructor called" << std::endl;
+    _unsorted = userInput;
 }
 
-PmergeMe::PmergeMe(const PmergeMe &source)
-{
-    std::cout << "\e[0;103mPmergeMe::\e[0m Copy constructor called" << std::endl;
-}
+PmergeMe::PmergeMe(const PmergeMe &source) : _oddNum(source._oddNum), _unsorted(source._unsorted), _sorted(source._sorted), _insertionSequence(source._insertionSequence) {}
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &source)
 {
     if (this != &source)
-        //code
+    {
+        _oddNum = source._oddNum;
+        _unsorted = source._unsorted;
+        _sorted = source._sorted;
+        _insertionSequence = source._insertionSequence;
+    }
 	
-    std::cout << "\e[0;103mPmergeMe::\e[0m Assignment operator used" << std::endl;
 	return (*this);
 }
 
-PmergeMe::~PmergeMe()
-{
-    std::cout << "\e[0;103mPmergeMe::\e[0m Destructor called" << std::endl;
-}
+PmergeMe::~PmergeMe() {}
 
 //////////////////////////////////////////////////////////////////////////////////////
 
 void PmergeMe::print(void) const
 {
-    for (int i = 0; _unsorted[i]; i++)
-        std::cout << "Before: " << _unsorted[i] << " ";
-    std::cout << std::endl;
-    
     for (int i = 0; _sorted[i]; i++)
-        std::cout << "After: " << _sorted[i] << " ";
+        std::cout << _sorted[i] << " ";
     std::cout << std::endl;
+}
 
+std::vector<int> generateInsertionSequence(int n)
+{
+    std::vector<int> jacobsthalSequence = generateJacobsthalSequence(n);
+
+    std::vector<int> insertionSequence;
+
+    insertionSequence[0] = jacobsthalSequence[3];
+    int lastJacobsNum = jacobsthalSequence[3];
+    for (int i = 1, j = 3; i < n; i++)
+    {
+        if (insertionSequence[i - 1] == jacobsthalSequence[j] && j != 3)
+        {
+            while()
+            insertionSequence[i] = 
+        }
+        else
+            insertionSequence[i] = jacobsthalSequence[3]
+    }
+}
+
+std::vector<int> generateJacobsthalSequence(int n)
+{
+    std::vector<int> jacobsthal(n);
+
+    if (n > 0) jacobsthal[0] = 0;
+    if (n > 1) jacobsthal[1] = 1;
+
+    for (int i = 2; i < n; ++i) {
+        jacobsthal[i] = jacobsthal[i-1] + 2 * jacobsthal[i-2];
+    }
+
+    return jacobsthal;
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -3,16 +3,32 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <deque>
+#include <sstream>
 
 class PmergeMe
 {
     private:
+        int _oddNum;
+        std::vector<int> _unsorted;
+        std::vector<int> _sorted;
+        std::vector<int> _insertionSequence;
+        
         void print(void) const;
+        std::vector<int> generateJacobsthalSequence(int n);
+        std::vector<int> generateInsertionSequence(int n);
+
     public:
-        PmergeMe();
+        PmergeMe(std::vector<int> &userInput);
         PmergeMe(const PmergeMe &source);
         PmergeMe &operator=(const PmergeMe &source);
-        ~PmergeMe();
+        ~PmergeMe(void);
+
+        void mergeInsertSort();
+        void print(void) const;
+        int getVecTime(void) const;
+        int getLstTime(void) const;
 };
 
 #endif // PMERGEME_HPP
