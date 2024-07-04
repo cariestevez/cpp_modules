6b99df7a65357c8fd7fcc10d7fa501ab7728160b
carina <carinaestevezorth@gmail.com>
2024-07-04 15:19:46 +0200

    sequence generation done

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -34,40 +34,58 @@ void PmergeMe::print(void) const
 std::vector<int> generateInsertionSequence(int n)
 {
     std::vector<int> jacobsthalSequence = generateJacobsthalSequence(n);
+    //int *jacobsthalSequence = generateJacobsthalSequence(argc, argv);
 
     std::vector<int> insertionSequence;
+    //int *insertionSequence = (int *)malloc(sizeof(int) * (argc - 1));
 
     insertionSequence[0] = jacobsthalSequence[3];
     insertionSequence[1] = jacobsthalSequence[3] - 1;
 
     int i = 1, j = 3;
+    int last = jacobsthalSequence[3];
 
-    for ( ; ++i < n;)
+    //for ( ; ++j, ++i < argc - 1;)
+    for ( ; ++j, ++i < n;)
     {
-        int last = j;
-        int num = jacobsthalSequence[last - 1];
-        j++;
-        while (num > jacobsthalSequence[last])
+        insertionSequence[i] = jacobsthalSequence[j];
+        int num = insertionSequence[i];
+        while (--num > last)
         {
-            insertionSequence[i] = num;
-            i++;
-            num--;
+            insertionSequence[++i] = num;
         }
-
+        last = jacobsthalSequence[j];
     }
 }
 
 std::vector<int> generateJacobsthalSequence(int n)
 {
     std::vector<int> jacobsthal(n);
+    // int *jacobsthal;
+    // jacobsthal = (int *)malloc(sizeof(int) * (argc - 1));
 
     if (n > 0) jacobsthal[0] = 0;
     if (n > 1) jacobsthal[1] = 1;
+    // if (argc - 1 > 0)
+    // {
+    //     jacobsthal[0] = 0;
+    //     std::cout << "0: " << jacobsthal[0] << " ";
+    // }
+    // if (argc - 1 > 1)
+    // {
+    //     jacobsthal[1] = 1;
+    //     std::cout << "1: " << jacobsthal[1] << " ";
+    // }
 
-    for (int i = 2; i < n + 2; ++i)//first 2 nums are not needed ('0', '1') so we generate 2 nums extra on their place. needed?
+    for (int i = 2; i < n; ++i)//first 2 nums are not needed ('0', '1') so we generate 2 nums extra on their place. needed?
     {
         jacobsthal[i] = jacobsthal[i-1] + 2 * jacobsthal[i-2];
     }
+    // for (int i = 2; i < argc; ++i)//first 2 nums are not needed ('0', '1') so we generate 2 nums extra on their place. needed?
+    // {
+    //     jacobsthal[i] = jacobsthal[i-1] + 2 * jacobsthal[i-2];
+    //     std::cout << i << ": " << jacobsthal[i] << " ";
+    // }
 
     return jacobsthal;
 }
