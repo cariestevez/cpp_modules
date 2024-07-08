7cc3714adb3d2c0de2dc2192d0c796a86f693c2f
carina <carinaestevezorth@gmail.com>
2024-07-08 11:24:02 +0200

    changing pc again

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,17 +1,56 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe(std::vector<int> &userInput)
+PmergeMe::PmergeMe(int argc, char **argv)
 {
-    _unsorted = userInput;
+    for (size_t i = 1; i < _size; i += 2)
+    {
+        // need to convert each arg(str into integer) and check it's valid. then saving the converted one into the pairs vector
+        // if (!isdigit(*argv[i]) || *argv[i] < 0)
+        //     throw (std::invalid_argument("Error: invalid input"));
+        int convertedFirst = isValidInt(argv[i]);
+        int convertedLast = isValidInt(argv[i + 1]);
+        if (!convertedFirst || !convertedLast)
+            throw (std::invalid_argument("Error: invalid input"));
+        if (i + 1 < _size && convertedFirst > convertedLast)
+        {
+            _pairs.push_back(std::make_pair(convertedFirst, convertedLast));
+        }
+        else if (i + 1 < _size && convertedLast > convertedFirst)
+        {
+            _pairs.push_back(std::make_pair(convertedLast, convertedFirst));
+        }
+        else
+        {
+            _oddNum = convertedFirst;
+        }
+    }
+
+    std::vector<int> unsortedVec;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::stringstream ss(argv[i]);
+        int num;
+        if (ss >> num && num >= 0)
+        {
+            unsortedVec.push_back(num);
+        }
+        else
+        {
+            std::cerr << "Error: Invalid input sequence." << std::endl;
+            return 1;
+        }
+    }
+    _size = _unsorted.size();
 }
 
-PmergeMe::PmergeMe(const PmergeMe &source) : _oddNum(source._oddNum), _unsorted(source._unsorted), _sorted(source._sorted), _insertionSequence(source._insertionSequence) {}
+PmergeMe::PmergeMe(const PmergeMe &source) : _oddNum(source._oddNum), _size(source._size), _unsorted(source._unsorted), _sorted(source._sorted), _insertionSequence(source._insertionSequence) {}
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &source)
 {
     if (this != &source)
     {
         _oddNum = source._oddNum;
+        _size = source._size;
         _unsorted = source._unsorted;
         _sorted = source._sorted;
         _insertionSequence = source._insertionSequence;
@@ -24,6 +63,11 @@ PmergeMe::~PmergeMe() {}
 
 //////////////////////////////////////////////////////////////////////////////////////
 
+void PmergeMe::mergeInsertSort()
+{
+    _insertionSequence = generateInsertionSequence();
+}
+
 void PmergeMe::print(void) const
 {
     for (int i = 0; _sorted[i]; i++)
@@ -31,41 +75,41 @@ void PmergeMe::print(void) const
     std::cout << std::endl;
 }
 
-std::vector<int> generateInsertionSequence(int n)
+std::vector<int> PmergeMe::generateInsertionSequence(void)
 {
-    std::vector<int> jacobsthalSequence = generateJacobsthalSequence(n);
+    std::vector<int> jacobsthalSequence = generateJacobsthalSequence();
     //int *jacobsthalSequence = generateJacobsthalSequence(argc, argv);
 
-    std::vector<int> insertionSequence;
+    //std::vector<int> insertionSequence;
     //int *insertionSequence = (int *)malloc(sizeof(int) * (argc - 1));
 
-    insertionSequence[0] = jacobsthalSequence[3];
-    insertionSequence[1] = jacobsthalSequence[3] - 1;
+    _insertionSequence[0] = jacobsthalSequence[3];
+    _insertionSequence[1] = jacobsthalSequence[3] - 1;
 
     int i = 1, j = 3;
     int last = jacobsthalSequence[3];
 
     //for ( ; ++j, ++i < argc - 1;)
-    for ( ; ++j, ++i < n;)
+    for ( ; ++j, ++i < _size;)
     {
-        insertionSequence[i] = jacobsthalSequence[j];
-        int num = insertionSequence[i];
+        _insertionSequence[i] = jacobsthalSequence[j];
+        int num = _insertionSequence[i];
         while (--num > last)
         {
-            insertionSequence[++i] = num;
+            _insertionSequence[++i] = num;
         }
         last = jacobsthalSequence[j];
     }
 }
 
-std::vector<int> generateJacobsthalSequence(int n)
+std::vector<int> PmergeMe::generateJacobsthalSequence(void)
 {
-    std::vector<int> jacobsthal(n);
+    std::vector<int> jacobsthal(_size);
     // int *jacobsthal;
     // jacobsthal = (int *)malloc(sizeof(int) * (argc - 1));
 
-    if (n > 0) jacobsthal[0] = 0;
-    if (n > 1) jacobsthal[1] = 1;
+    if (_size > 0) jacobsthal[0] = 0;
+    if (_size > 1) jacobsthal[1] = 1;
     // if (argc - 1 > 0)
     // {
     //     jacobsthal[0] = 0;
@@ -77,7 +121,7 @@ std::vector<int> generateJacobsthalSequence(int n)
     //     std::cout << "1: " << jacobsthal[1] << " ";
     // }
 
-    for (int i = 2; i < n; ++i)//first 2 nums are not needed ('0', '1') so we generate 2 nums extra on their place. needed?
+    for (int i = 2; i < _size; ++i)//first 2 nums are not needed ('0', '1') so we generate 2 nums extra on their place. needed?
     {
         jacobsthal[i] = jacobsthal[i-1] + 2 * jacobsthal[i-2];
     }
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -8,41 +8,33 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    std::vector<int> unsortedVec;
-    for (int i = 1; i < argc; ++i)
+    try
     {
-        std::stringstream ss(argv[i]);
-        int num;
-        if (ss >> num && num >= 0)
-        {
-            unsortedVec.push_back(num);
-        }
-        else
-        {
-            std::cerr << "Error: Invalid input sequence." << std::endl;
-            return 1;
-        }
+        PmergeMe sortMe(argc, argv);
+
+        std::cout << "Before: ";
+        for (int i = 1; i < argc; i++)
+            std::cout << *argv[i] << " ";
+        std::cout << std::endl;
+
+        sortMe.mergeInsertSort();
+
+        // // Sort using std::list
+        // std::list<int> numList(numbers.begin(), numbers.end());
+        // PmergeMe::mergeInsertSort(numList);
+
+        std::cout << "After: ";
+        sortMe.print();
+
+        std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector: " << sortMe.getVecTime() << std::endl;
+        std::cout << "Time to process a range of " << argc - 1 << " elements with std::list: " << sortMe.getLstTime() << std::endl;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
     
-    PmergeMe sortMe(unsortedVec);
-
-    std::cout << "Before: ";
-    for (std::vector<int>::size_type i = 0; i < unsortedVec.size(); i++)
-        std::cout << unsortedVec[i] << " ";
-    std::cout << std::endl;
-
-    sortMe.mergeInsertSort();
-
-    // // Sort using std::list
-    // std::list<int> numList(numbers.begin(), numbers.end());
-    // PmergeMe::mergeInsertSort(numList);
-
-    std::cout << "After: ";
-    sortMe.print();
-
-    std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector: " << sortMe.getVecTime() << std::endl;
-    std::cout << "Time to process a range of " << argc - 1 << " elements with std::list: " << sortMe.getLstTime() << std::endl;
-
     return 0;
 }
 
