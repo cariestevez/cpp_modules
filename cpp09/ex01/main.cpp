653de4f6b1d73a0f9523438e8ce215206ddf40b2
Carina Estevez Orth <carinaestevezorth@gmail.com>
2024-07-15 18:57:54 +0200

    08 evaluated. still working on 09 ex02 :'(

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -2,37 +2,14 @@
 
 int main(int argc, char **argv)
 {
-    int *jacobsthalSequence = generateJacobsthalSequence(n);
-
-    std::vector<int> insertionSequence;
-
-    insertionSequence[0] = jacobsthalSequence[3];
-    insertionSequence[1] = jacobsthalSequence[3] - 1;
-
-    int i = 1, j = 3;
-
-    for ( ; ++i < n;)
+    if (argc != 2)
     {
-        int last = j;
-        int num = jacobsthalSequence[last - 1];
-        j++;
-        while (num > jacobsthalSequence[last])
-        {
-            insertionSequence[i] = num;
-            i++;
-            num--;
-        }
-
+        std::cerr << "Usage: " << argv[0] << " <RPN expression>" << std::endl;
+        return 1;
     }
-}
-    // if (argc != 2)
-    // {
-    //     std::cerr << "Usage: " << argv[0] << " <RPN expression>" << std::endl;
-    //     return 1;
-    // }
     
-    // RPN rpnCalculator(argv[1]);
-    // rpnCalculator.calculateExpression();
+    RPN rpnCalculator(argv[1]);
+    rpnCalculator.calculateExpression();
 
-    // return 0;
+    return 0;
 }
